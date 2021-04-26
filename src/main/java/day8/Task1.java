4dac85d98ddb76549c385ba1b7f5638cd5aabd6e
Valeria Karpocheva <lera.karpocheva@mail.ru>
2021-04-26 22:36:33 +0300

    add day 8 Task 1 corrected

--- src/main/java/day8/Task1.java
+++ src/main/java/day8/Task1.java
@@ -2,15 +2,26 @@ package day8;
 
 public class Task1 {
     public static void main(String[] args) {
+        int i;
+        String string = " ";
+        final long startTime1 = System.currentTimeMillis();
+        for (i = 0; i < 20001; i++)
+            string += i + " ";
+        System.out.println(string);
+        final long endTime1 = System.currentTimeMillis(); //Total execution time: 1211
+
+        System.out.println("Total execution time: " + (endTime1 - startTime1));
+
         StringBuilder sb = new StringBuilder(" ");
-        int i = 0;
+
         final long startTime = System.currentTimeMillis();
         for (i = 0; i < 20001; i++) {
             sb.append(i + " ");
         }
         System.out.println(sb);
-        final long endTime = System.currentTimeMillis();
+        final long endTime = System.currentTimeMillis(); //Total execution time: 45
 
         System.out.println("Total execution time: " + (endTime - startTime));
     }
 }
+
